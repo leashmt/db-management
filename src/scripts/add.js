a7f4f3ff6e19a71debabba93b4ebe47c3d0292eb
leaschmt <lea.schmitt@supdevinci-edu.fr>
2025-01-13 20:48:43 +0100

    Refactor addData function to handle database transactions and improve error handling

--- src/scripts/add.js
+++ src/scripts/add.js
@@ -13,26 +13,37 @@ const dbConfig = {
 };
 
 async function addData() {
+	let connection;
+
 	try {
 		if (!fs.existsSync('./src/scripts/add.sql')) {
 			console.error("Le fichier 'add.sql' est introuvable.");
-			return;
+			return false;
 		}
-		const connection = await mysql.createConnection(dbConfig);
+
 		const schema = fs.readFileSync('./src/scripts/add.sql', 'utf8');
+
+		connection = await mysql.createConnection(dbConfig);
+
+		await connection.beginTransaction();
+
 		await connection.query(schema);
-		await connection.end();
+
+		await connection.commit();
+
+		console.log('Les données ont été ajoutées avec succès.');
 		return true;
 	} catch (error) {
+		if (connection) await connection.rollback();
 		console.error("Erreur lors de l'ajout des données :", error.message);
 		return false;
+	} finally {
+		if (connection) await connection.end();
 	}
 }
 
 addData().then(success => {
-	if (success) {
-		console.log('Les données ont été ajoutées avec succès.');
-	} else {
+	if (!success) {
 		console.log("Échec de l'ajout des données.");
 	}
 });
